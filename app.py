5c4f6921180fa884214f4652ebcd0cd368e2b7a9
prakash <prakashsellathurai@gmail.com>
2020-10-31 21:57:40 +0530

    updated Ui

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 import json
 import logging
 import os
+
 
 from sklearn.externals import joblib
 
@@ -12,25 +13,25 @@ MODEL = os.path.join(APP_ROOT, 'classifier.pkl')
 
 PORT = 5000
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='views')
 logging.basicConfig(filename='movie_classifier.log', level=logging.DEBUG)
 model = joblib.load(MODEL)
-label = {0:'negative', 1:'positive'}
+label = {0: 'negative', 1: 'positive'}
 
 
 @app.route('/')
 def home():
-    return 'It works.'
+    return render_template('home.html')
 
 
 def predict(model, text):
     return label[model.predict([text])[0]]
 
 
-@app.route('/review', methods=['GET'])
+@app.route('/review', methods=['GET', 'POST'])
 def extract():
     """Return the movie review sentiment score.
-    
+
     Returns a JSON object :
     {
          "sentiment": "positive"
@@ -38,14 +39,23 @@ def extract():
     """
     if request.method == 'GET':
         description = request.args.get('text', '')
-        
+
         result = {
             'sentiment': predict(model, description)
         }
-        return json.dumps(result) 
+        return json.dumps(result)
+    elif request.method == 'POST':
 
+
+        data = request.json
+
+        description = (type(data['input-text']) !=  "undefined") and data['input-text'] or ""
+        result = {
+            'sentiment': predict(model, description)
+        }
+        print("proceesed")
+        return json.dumps(result)
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=PORT)
-
+    app.run(host='0.0.0.0', port=PORT, debug=True)
